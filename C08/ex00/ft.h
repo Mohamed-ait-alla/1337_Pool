88cd2af3e7f262b13b7310b60eacaba75a0ddd36
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2024-09-26 23:08:46 +0100

    Change

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mait-all <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 18:54:26 by mait-all          #+#    #+#             */
+/*   Updated: 2024/09/26 18:55:07 by mait-all         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
